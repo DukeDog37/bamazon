41f973feeb9ec38314667927c6b118dc599ba4d0
Jason H <jahenry@gwmail.gwu.edu>
2017-07-26 19:41:26 -0400

    all required functionality in place. Pre-integration of CLI-TABLE npm package

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -45,11 +45,93 @@ takeAction();
 function fnViewProductSales(){
 
 	console.log("in the view prod sales function");
+	mysql.getConnection(function(err, conn){
+		
+		  if (err) throw err;
+		  var sql = "SELECT department_id, department_name, over_head_costs, product_sales, " +
+		  "(product_sales - over_head_costs) as total_profit from departments";
+		  conn.query(sql, function (err, result) {
+		    if (err) throw err;
+		    //output results formatted nicely
+		    console.log("=============DEPARTMENT SALES SUMMARY====================");
+		    console.log("ID             Department Name          Overhead Costs          Product Sales         Total Profit");
+		    for(i = 0; i < result.length; i++){
+		    	console.log(result[i].department_id + "     " + result[i].department_name + "     " + result[i].over_head_costs + "         " + result[i].product_sales + "        " + result[i].total_profit);
+		    }
+		    if(conn){
+			 	conn.release();
+			 	mysql.end();
+			 	
+			}
+			});
+
+		});
+
 
 
 }
 
 function fnCreateNewDept(){
 
-	console.log("in the create new department function");
+	mysql.getConnection(function(err, conn){
+		if (err){
+			throw err;
+		} 
+		var sql = "SELECT department_name FROM departments";
+		conn.query(sql, function (err, result) {
+			if (err){
+				throw err;
+			} 
+			else{
+				//populate array of departments to select from
+				var strDepts = [];
+			    for(i = 0; i < result.length; i++){
+			    	//console.log(result[i].department_name);
+				    strDepts.push(result[i].department_name);
+			    }
+			}
+		
+		inquirer
+			.prompt([
+			{
+				type: "input",
+				message: "What is the name of the new department?",
+				name: "deptname"
+			},
+			{
+				type: "input",
+				message: "What are the Overhead Costs associated with this department?",
+				name: "overhead"
+			},
+			{
+				type: "confirm",
+				message: "Confirm to add new department. Sales will be initially set to zero.",
+				name: "confirm",
+				default: true
+				}]).then(function(inquirerResponse) {
+				if (inquirerResponse.confirm == true) {
+					var sql = "insert into departments(department_name, over_head_costs, product_sales) values( " +
+					"'" + inquirerResponse.deptname + "', '" + inquirerResponse.overhead + "', 0.00)"; 
+					conn.query(sql, function (err, result) {
+				   	if (err){
+				   		throw err;
+				   	} 
+				   	else{
+				   		console.log(inquirerResponse.deptname + " has been added to the departments database.");
+				   	}
+				   	if(conn){
+						 	conn.release();
+						 	mysql.end();
+						 	
+					}
+				});
+			}
+		});
+});
+
+
+		
+
+	});
+
 }
